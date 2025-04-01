914202fa6a5b8825a81db789b3ca849a6f26ce9c
Pak_Man926 <davidkamau926@gmail.com>
2025-04-01 14:06:00 +0300

    Switch

---
+++ switch_app/lib/main.dart
@@ -0,0 +1,62 @@
+import "package:flutter/material.dart";
+
+void main()
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      home: ToggleSwitch()
+    );
+  }
+}
+
+class ToggleSwitch extends StatefulWidget
+{
+  _ToggleSwitchState createState() => _ToggleSwitchState();
+}
+
+class _ToggleSwitchState extends State<ToggleSwitch>
+{
+  bool isSwitched = false;
+  
+  void _toggleSwitch(bool value)
+  {
+    setState(()
+             {
+               isSwitched = value;
+             }
+            );
+  }
+  
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Switcher App")
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              isSwitched ? "Switch is On" : "Switch is Off",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w200)
+            ),
+            Switch(
+              value: isSwitched,
+              onChanged: _toggleSwitch,
+            )
+          ]
+        )
+      )
+    );
+  }
+}
